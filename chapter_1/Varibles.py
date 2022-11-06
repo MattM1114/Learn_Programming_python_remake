d8e055f2ff00364ddde39a23ea400d3f84f5ae80
Matthew Tadiwa Magadza <64587393+MattM1114@users.noreply.github.com>
2022-11-06 17:31:40 +0200

    answers for the first three chapters

---
+++ chapter_1/Varibles.py
@@ -0,0 +1,7 @@
+#which of the following are invalid variable declarations in Python?
+# select all that apply.
+
+#these are the correct ans
+
+None = ""
+1_plus_2 = "three"
---
+++ chapter_1/strings.py
@@ -0,0 +1,28 @@
+# q1
+# What is the value of the ans after the following code executes?
+res1 = "Programming is fun!"
+ans1 = res1[5]
+
+# the answer is "a" .
+
+# q2
+# what is the value of the variable ans after the following code executes?
+res2 = "Programming is fun!"
+ans2 = res2[1:5]
+
+# the answer is "rogr".
+
+# q3
+# What is the value of the variable ans after the following code executes?
+res3 = "12 is > than 1."
+ans3 = res3[-6]
+
+# the answer "h".
+
+# q4
+# what is the value of the variable ans after the following code executes?
+res4 = "12 is > than 1."
+ans4 = res4[-5:-1]
+
+# the answer is "an 1".
+
