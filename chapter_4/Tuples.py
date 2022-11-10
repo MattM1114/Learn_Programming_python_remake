87abb27fb75b30ce7dd3603ea78ea74a7fc54970
Matthew Tadiwa Magadza <64587393+MattM1114@users.noreply.github.com>
2022-11-10 18:19:19 +0200

    Create Tuples.py

---
+++ chapter_4/Tuples.py
@@ -0,0 +1,10 @@
+# let's say we've declared your_tuple as shown below:
+your_tuple = ("Hello", "friends")
+# which of the following statements will cause an error? 
+your_tuple = your_tuple + 1
+your_tuple[0] += 1
+your_tuple.append("!")
+# these will
+
+# this won't
+your_tuple = ("hello") 
