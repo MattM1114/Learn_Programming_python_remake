b286a9aefb342407662d11cd55ee2357ad9a76a1
Matthew Tadiwa Magadza <64587393+MattM1114@users.noreply.github.com>
2022-11-13 17:15:39 +0200

    Create dicts.py

---
+++ chapter_4/dicts.py
@@ -0,0 +1,6 @@
+#how many many elements will be in your_dict after the following code executes
+keys = [ "red", "blue", "green", "yellow", "green", "purple", "blue"]
+your_dict = dict()
+for key in keys:
+    your_dict[key] = None
+print(your_dict)
